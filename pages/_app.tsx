9f5658ded80e89d49af1e80f1a38e20f88db7ee3
Douglas S. Camargo <douglas@nibbletecnologia.com.br>
2020-09-26 18:34:31 -0300

    personal website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
+import Head from 'next/head';
 import { AppProps } from 'next/app';
-import '../styles/global.css';
+
+import GlobalStyle from '../styles/global';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
